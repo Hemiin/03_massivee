aa50ea85b102ac6617ca0d3983a083805fbf7370
HemiinP <sdfnli@gmail.com>
2025-09-29 13:16:07 +0300

    z

--- src/Base/3_5.kt
+++ src/Base/3_5.kt
@@ -1,5 +1,33 @@
 package Base
 
-fun main(){
+fun main() {
+    println("Введите слова: ")
+    val input = readLine()
+    if (input.isNullOrBlank()) {
+        println("Ввод пустой")
+        return
+    }
 
+    val words = input.split(" ").map { it.trim() }
+    val groupedWords = groupWord(words)
+
+    println("Результат: ")
+    for (i in groupedWords){
+        println(i)
+    }
+}
+
+fun groupWord(words: List<String>): List<List<String>> {
+    val groups = mutableMapOf<String, MutableList<String>>()
+
+    for (word in words) {
+        val key = word.toCharArray().sorted().joinToString("")
+
+        if (!groups.containsKey(key)) {
+            groups[key] = mutableListOf()
+        }
+        groups[key]?.add(word)
+    }
+
+    return groups.values.toList()
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    println("Введите слова через запятую. Пример ate,tea,tan")
+    println("Ввод:")
+    val inpuuut: String = readln()
+    val result = Sorteed(inpuuut)
+
+    println("Результат: ")
+    for (i in result) {
+//        println(i)
     }
+}
+
+fun Sorteed(inpuuut: String): List< String>{
+
+    var wwod = inpuuut.split("").map { it.trim() }.filter { it.isNullOrBlank() }
+    var charList = StringBuilder()
+    val listik = mutableListOf<String>()
+
+    listik.add(charList.toString())
+    val yyy = mutableMapOf<String, MutableList<String>>()
+    for( i in listik){
+        var arr = i.toCharArray()
+
+        println(arr.sorted().toString())
+//        yyy.put(arr.toString(), (i))
+    }
+
+//    val result = List<String>
+//    return result
+    return listik
 }
