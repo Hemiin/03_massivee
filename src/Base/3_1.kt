bc98e803e6dbdc2bf69043db0ae354dfb0de6d33
Hemiin <DASP@gmail.com>
2025-10-16 01:25:35 +0300

    z

---
+++ src/Base/3_1.kt
@@ -0,0 +1,9 @@
+package Base
+
+fun main(){
+    println("Введите количество строи и столбцов двухмерного массива в виде: x y")
+    var a = readln().split(" ")
+    println(a)
+    println("Вводите числа массива в виде: z z1 z2...")
+    var b = readln().split(" ")
+}
